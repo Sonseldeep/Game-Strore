3847fd931713368f8d8e3cbcaa8333d9cbc24b60
Sandeep Shrestha <sonseldeep8890.shrestha@gmail.com>
2025-06-16 22:00:58 +0545

    Update NuGet package cache with new dependencies and versions

--- obj/Debug/net8.0/GameStor.Api.AssemblyInfo.cs
+++ obj/Debug/net8.0/GameStor.Api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GameStor.Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2fa84c8bea090ef4688c9ae95b8a88b22dd0f086")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7bba5c5638b4439db589bd75f8db4751c62b3c95")]
 [assembly: System.Reflection.AssemblyProductAttribute("GameStor.Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GameStor.Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- run.sh
+++ run.sh
@@ -6,6 +6,8 @@ set -e
 echo "📦 Installing NuGet dependencies..."
 dotnet add package MinimalApis.Extensions --version 0.11.0
 
+
+dotnet add package Microsoft.EntityFrameworkCore.Sqlite --version 8.0.2
 echo "🔧 Building the project..."
 dotnet build
 
