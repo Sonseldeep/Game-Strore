2c5e4776e8fbaba640253579664e8148a2de19da
Sandeep Shrestha <sonseldeep8890.shrestha@gmail.com>
2025-06-18 13:45:57 +0545

    fix: update AssemblyInformationalVersion and cache for consistency

--- obj/Debug/net8.0/GameStor.Api.AssemblyInfo.cs
+++ obj/Debug/net8.0/GameStor.Api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GameStor.Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2f62a138596823a722094e76a5dc80407f04edfd")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c5063bc80ed2754d46a0edc521adcb6aff4df264")]
 [assembly: System.Reflection.AssemblyProductAttribute("GameStor.Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GameStor.Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
