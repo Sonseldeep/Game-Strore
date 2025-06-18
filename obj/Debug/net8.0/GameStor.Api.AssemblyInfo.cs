ddd800623585708c258610fbe8ef4f6b861e4a07
Sandeep Shrestha <sonseldeep8890.shrestha@gmail.com>
2025-06-18 17:15:02 +0545

    fix: remove DataMigrations directory and reinstall inside Data directory for readiability

--- obj/Debug/net8.0/GameStor.Api.AssemblyInfo.cs
+++ obj/Debug/net8.0/GameStor.Api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GameStor.Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+480565664df390f7e7f1b0f591c58d623811237d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+542571cf0edd14f8946c9cd5ac75e2050331f278")]
 [assembly: System.Reflection.AssemblyProductAttribute("GameStor.Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GameStor.Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
