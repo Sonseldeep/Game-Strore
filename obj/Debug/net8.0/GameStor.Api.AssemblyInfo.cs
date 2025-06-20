140969560c53a8db9339f24e17035abb16c5b05f
Sandeep Shrestha <sonseldeep8890.shrestha@gmail.com>
2025-06-20 22:52:20 +0545

    chore: update binary files and assembly information for GameStor.Api

--- obj/Debug/net8.0/GameStor.Api.AssemblyInfo.cs
+++ obj/Debug/net8.0/GameStor.Api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GameStor.Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+76fea0b1aef3a03a5b73df3d2c5d94a40b73b675")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f7b8364f4ef4ef1c4a45bb1741656eb5bdac2bc8")]
 [assembly: System.Reflection.AssemblyProductAttribute("GameStor.Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GameStor.Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
