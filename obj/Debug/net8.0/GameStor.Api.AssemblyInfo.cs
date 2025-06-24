59fc5e6d1a900cb7eb20b4d7780db1c0c6376efe
Sandeep Shrestha <sonseldeep8890.shrestha@gmail.com>
2025-06-24 21:15:29 +0545

    chore: update assembly information and add dotnet-ef tool configuration

--- obj/Debug/net8.0/GameStor.Api.AssemblyInfo.cs
+++ obj/Debug/net8.0/GameStor.Api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GameStor.Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f7b8364f4ef4ef1c4a45bb1741656eb5bdac2bc8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+140969560c53a8db9339f24e17035abb16c5b05f")]
 [assembly: System.Reflection.AssemblyProductAttribute("GameStor.Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GameStor.Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
